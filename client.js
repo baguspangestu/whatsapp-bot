16d8001766d2c7833bd40b45f14034a44e74a8b4
Bagus Pangestu <baguspangestu@yandex.com>
2023-04-13 19:19:19 +0700

    Menambahkan LocalAuth dan Save File Local

--- client.js
+++ client.js
@@ -1,7 +1,23 @@
-const { Client } = require("whatsapp-web.js");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
+const fs = require("fs");
+const path = require("path");
 
-const client = new Client();
+const client = new Client({ authStrategy: new LocalAuth() });
+
+const downloadFile = async (msg) => {
+  const attachmentData = await msg.downloadMedia();
+
+  const storagePath = path.join(__dirname, "images");
+  const extension = attachmentData.mimetype.split("/")[1];
+  const fileName = `${msg.id.id}.${extension}`;
+
+  fs.writeFileSync(
+    `${storagePath}/${fileName}`,
+    Buffer.from(attachmentData.data, "base64").toString("binary"),
+    "binary"
+  );
+};
 
 client.on("qr", (qr) => {
   console.log("Scan QR Code Untuk Login:");
@@ -12,7 +28,11 @@ client.on("ready", () => {
   console.log("BOT Siap!");
 });
 
-client.on("message", (msg) => {
+client.on("message", async (msg) => {
+  if (msg.hasMedia) {
+    downloadFile(msg);
+  }
+
   if (msg.body == "!ping") {
     msg.reply("pong");
   }
